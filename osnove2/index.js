a0a03340d2e47caea246f88f00c438a87758ed9a
Luka <nluka9943@gmail.com>
2026-03-31 23:59:01 +0200

    osnove2

---
+++ osnove2/index.js
@@ -0,0 +1,11 @@
+let email = "ll123345@gmail.com";
+
+//da li sadrzi @
+console.log(email.includes("@"));
+//daj sve karaktere od 0 do 5 pozicije,
+//ne ukljucujuci 5 poziciju
+console.log(email.substring(0, 5));
+//duzina stringa
+console.log(email.length);
+//na kom je indeksu @
+console.log(email.indexOf("@"));
